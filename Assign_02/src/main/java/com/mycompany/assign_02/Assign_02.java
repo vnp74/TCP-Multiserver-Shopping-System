1137a1ae46c472b79ed8ec609618b818815802df
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-04-18 23:13:36 +1000

    project created

---
+++ Assign_02/src/main/java/com/mycompany/assign_02/Assign_02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assign_02;
+
+/**
+ *
+ * @author VRAJ
+ */
+public class Assign_02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
