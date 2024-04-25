c553c66f4ce8a902dfcbbe5502176aec43de14b8
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-04-25 21:43:51 +1000

    initialize ServerCoordinator
    Initial setup of ServerCoordinator with ServerSocket initialization.

--- Assign_02/src/main/java/com/mycompany/assign_02/ServerCoordinator.java
+++ Assign_02/src/main/java/com/mycompany/assign_02/ServerCoordinator.java
@@ -4,10 +4,34 @@
  */
 package com.mycompany.assign_02;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author VRAJ
  */
 public class ServerCoordinator {
-    
+    private ServerSocket serverSocket;
+
+    public ServerCoordinator(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("ServerCoordinator running on port " + port);
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerCoordinator coordinator = new ServerCoordinator(8000);
+            coordinator.start();
+        } catch (IOException e) {
+            System.err.println("ServerCoordinator failed to start: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    public void start() {
+        System.out.println("ServerCoordinator is waiting for the connection...");
+    }
 }
