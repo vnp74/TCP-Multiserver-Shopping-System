52e4f9c50b667b45e67283fcdd3772f820fb88ff
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-04-25 15:41:26 +1000

    implement MovieOrder class

--- Assign_02/src/main/java/com/mycompany/assign_02/MovieOrder.java
+++ Assign_02/src/main/java/com/mycompany/assign_02/MovieOrder.java
@@ -4,10 +4,35 @@
  */
 package com.mycompany.assign_02;
 
+import java.io.Serializable;
+
 /**
  *
  * @author VRAJ
  */
-public class MovieOrder {
-    
+public class MovieOrder implements Task, Serializable {
+    private int quantity;
+    private double unitPrice;
+    private double tax;
+    private double totalBill;
+    private static final double TAX_RATE = 0.30;
+
+    public MovieOrder(int quantity, double unitPrice) {
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.tax = 0;
+        this.totalBill = 0;
+    }
+
+    @Override
+    public void executeTask() {
+        double subtotal = quantity * unitPrice;
+        tax = subtotal * TAX_RATE;
+
+    }
+
+    @Override
+    public String getResult() {
+        return "Number of Movies: " + quantity + "Price: " + unitPrice + "Tax: " + tax + "Total Bill: " + totalBill;
+    }
 }
