99bdc8710591f7578e9302a811ba61103dd08c1d
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-04-25 17:30:44 +1000

    basic orderclient structure

--- Assign_02/src/main/java/com/mycompany/assign_02/OrderClient.java
+++ Assign_02/src/main/java/com/mycompany/assign_02/OrderClient.java
@@ -4,10 +4,31 @@
  */
 package com.mycompany.assign_02;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author VRAJ
  */
 public class OrderClient {
-    
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public OrderClient(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public static void main(String[] args) {
+        try {
+            OrderClient client = new OrderClient("localhost", 8000);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
