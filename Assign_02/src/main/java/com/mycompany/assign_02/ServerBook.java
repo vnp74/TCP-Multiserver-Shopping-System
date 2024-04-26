e98d8d3114fb40b3daae665376d5dc5307083fdb
Vnp74 <68412685+vnp74@users.noreply.github.com>
2024-04-26 16:24:40 +1000

    initialize ServerBook
    Initial setup of ServerBook with ServerSocket initialization.

--- Assign_02/src/main/java/com/mycompany/assign_02/ServerBook.java
+++ Assign_02/src/main/java/com/mycompany/assign_02/ServerBook.java
@@ -4,10 +4,33 @@
  */
 package com.mycompany.assign_02;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author VRAJ
  */
 public class ServerBook {
-    
+    private ServerSocket serverSocket;
+
+    public ServerBook(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("ServerBook running on port" + port);
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerBook serverBook = new ServerBook(8001);
+            serverBook.start();
+        } catch (IOException e) {
+            System.err.println("ServerBook failed to Start: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+
+    }
 }
